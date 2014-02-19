785aee29efa553917fbbc18d2b9658d4c57caca1
Apprentice <apprentice@dbc-chicago-017-67.local>
2014-02-18 18:40:51 -0600

    /

--- sinatra_skeleton/app/controllers/index.rb
+++ sinatra_skeleton/app/controllers/index.rb
@@ -1,4 +1,12 @@
 get '/' do
   # Look in app/views/index.erb
+  @categories = Category.all.to_a
   erb :index
+end
+
+get '/categories/:id' do
+  @category = Category.find_by(id: params[:id])
+
+  @posts = @category.posts
+  erb :category
 end
